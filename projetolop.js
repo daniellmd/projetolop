238c76fd54dcea76f9ed821a313eecab8ad710d7
daniellmd <30603711+daniellmd@users.noreply.github.com>
2017-09-25 15:46:59 -0300

    Etapa 1/2

---
+++ projetolop.js
@@ -0,0 +1,35 @@
+<script>
+// Mova seu mouse na janela ao lado 
+// para ver o processing em ação
+var x = 160;
+var y = 160;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  clear ();
+  fill(64, 224, 0);
+  rect(100, 100, 80, 80);
+
+  
+ if (keyIsDown(LEFT_ARROW))
+    x-=5;
+
+  if (keyIsDown(RIGHT_ARROW))
+    x+=5;
+
+  if (keyIsDown(UP_ARROW))
+    y-=5;
+
+  if (keyIsDown(DOWN_ARROW))
+    y+=5;
+
+  fill(255, 0, 0);
+  ellipse(x, y, 50, 50);
+}
+
+
+
+</script>
